7b3713c3e0a33017ff9151e79cc7736f2eda2407
olena-nikitchenko <helnikitka@gmail.com>
2023-09-13 21:58:28 +0300

    code refactoring

--- src/shared/components/App.jsx
+++ src/shared/components/App.jsx
@@ -1,8 +1,9 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 // import { routes } from "../routes";
 import HomePage from "../../pages/HomePage/HomePage";
 import CatalogPage from "../../pages/CatalogPage/CatalogPage";
 import FavoritesPage from "../../pages/FavoritePage/FavoritePage";
+import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
 
 export default function App() {
 	return (
@@ -10,19 +11,18 @@ export default function App() {
 			<Route
 				path="/"
 				element={<HomePage />}
-			>
-				<Route
-					path="/catalog"
-					element={<CatalogPage />}
-				/>
-				<Route
-					path="/favorites"
-					element={<FavoritesPage />}
-				/>
-			</Route>
+			/>
+			<Route
+				path="/catalog"
+				element={<CatalogPage />}
+			/>
+			<Route
+				path="/favorites"
+				element={<FavoritesPage />}
+			/>
 			<Route
 				path="*"
-				element={<Navigate to="/" />}
+				element={<NotFoundPage />}
 			/>
 		</Routes>
 	);

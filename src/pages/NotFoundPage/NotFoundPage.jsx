7f116f35dca5daaa66886131b22fa5337f0c7450
olena-nikitchenko <helnikitka@gmail.com>
2023-09-16 23:55:17 +0300

    added notFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import NotFoundImg from "../../images/NotFound.jpeg";
+import css from "./NotFoundPage.module.scss";
 
 export default function HomePage() {
-	return <>NotFoundPage</>;
+	return (
+		<div>
+			<h1 className={css.title}>Sorry page not found</h1>
+			<img
+				className={css.img}
+				src={NotFoundImg}
+				alt="Page not found"
+			/>
+		</div>
+	);
 }

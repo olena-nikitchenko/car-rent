cf010c9822d35df96f6704a326679e8442ca73e8
olena-nikitchenko <helnikitka@gmail.com>
2024-03-12 22:05:00 +0200

    changed image to homePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import carHome from "../../images/carHome.jpg";
 import css from "./HomePage.module.scss";
 
 export default function HomePage() {
 	return (
-		<div>
-			<h1 className={css.title}>Car rental - find, compare and save</h1>
-			<img
-				className={css.img}
-				src={carHome}
-				alt="Cars for rent"
-			/>
+		<div className={css.home}>
+			<h1 className={css.title}>Car rental</h1>
 		</div>
 	);
 }
